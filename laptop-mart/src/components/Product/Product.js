7658c401a1e06e0632d12e4768cfaf980655c56e
Somen Singha <somensingha06@gmail.com>
2022-03-26 13:16:23 +0600

    added

---
+++ laptop-mart/src/components/Product/Product.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+const Product = ({ product }) => {
+  const { name, price, image, id } = product;
+  return (
+    <div>
+      <div className="card my-3" style={{ width: "18rem" }}>
+        <img className="card-img-top p-2" src={image} alt="Card image cap" />
+        <div className="card-body">
+          <p className="card-text">Product Id: {id}</p>
+          <p className="card-text">Product Name: {name}</p>
+          <p className="card-text">Product Price: ${price}</p>
+          <button className="rounded">
+            ADD TO CART <FontAwesomeIcon icon={faShoppingCart} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
